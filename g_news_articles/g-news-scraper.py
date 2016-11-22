4fd5cbfe7a755fe5748bfb2b3c3e6653a480193a
mzhang0 <mzhang0125@gmail.com>
2016-11-22 10:02:51 -0500

    Added newly scraped g news articles.

--- g_news_articles/g-news-scraper.py
+++ g_news_articles/g-news-scraper.py
@@ -4,13 +4,13 @@ import sys
 
 user_agent = "Mozilla/5.0 (Windows NT x.y; WOW64; rv:10.0) Gecko/20100101 Firefox/10.0"
 urls = []
-results = []
+results = set()
 
 if len(sys.argv) != 2:
 	print("Usage: <list-of-urls.txt>")
 	sys.exit()
 
-with open(sys.argv[1]) as infile:
+with open(sys.argv[1],'r') as infile:
 	for line in infile:
 		urls.append(line.strip('\n'))
 
@@ -18,12 +18,13 @@ for url in urls:
 	page = requests.get(url, headers={'User-Agent': user_agent, 'Upgrade-Insecure-Requests': '1'}, allow_redirects=True).content
 	soup = BeautifulSoup(page, "lxml")
 
-	for anc_pri in soup.findAll('a', attrs={'class':'l _HId'}):
-		results.append(anc_pri['href'])
+	for anc_primary in soup.findAll('a', attrs={'class':'l _HId'}):
+		results.add(anc_primary['href'])
 
-	for anc_sec in soup.findAll('a', attrs={'class':'_sQb'}):
-		results.append(anc_sec['href'])
+	for anc_secondary in soup.findAll('a', attrs={'class':'_sQb'}):
+		results.add(anc_secondary['href'])
 
-	with open('news-articles.txt','w') as outfile:
-		for link in results:
-			outfile.write(link + '\n')
+results = list(results)
+with open('news-articles1.txt','w') as outfile:
+	for link in results:
+		outfile.write(link + '\n')
