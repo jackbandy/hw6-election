f35d6247c453469246b9caf329b37b47f98f28c5
Jack Bandy <jack.bandy@my.wheaton.edu>
2016-12-15 10:48:38 -0500

    Updated electspider.py

--- electspider.py
+++ electspider.py
@@ -7,35 +7,162 @@ and add edges to our graph if any hits occur
 
 import scrapy
 from scrapy.crawler import CrawlerRunner
+from scrapy.http import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.log import configure_logging
+from twisted.internet import reactor
 from bs4 import BeautifulSoup
+import numpy as np
+import pickle
 
 def main():
+    global articles
+    global polls
+    global sublinks
+    global link_dict
+    global poll_link_count
+    global total_crawl_count
+    global poll_tally
+
+    link_dict = {}
+    poll_link_count = 0
+    total_crawl_count = 0
+    d_articles = {}
+    articles = []
+
+    raw_polls = np.genfromtxt('poll_list/national_poll_urls.txt',
+            dtype=str, delimiter='\n')
+    raw_s_polls= np.genfromtxt('poll_list/state_poll_urls.txt',
+            dtype=str, delimiter='\n')
+    n_polls = [x[:x.find(',')].strip() for x in raw_polls]
+    print("{} national polls".format(len(n_polls)))
+    s_polls = [x[:x.find(',')].strip() for x in raw_s_polls]
+    print("{} state polls".format(len(s_polls)))
+    polls = n_polls + s_polls
+    print("{} total polls".format(len(polls)))
+    poll_tally = dict.fromkeys(polls)
+    for l in polls:
+        poll_tally[l] = 0
+
+    print('loading articles...')
+    prev_articles_r = np.genfromtxt('all_articles.txt',
+            dtype=str, delimiter='\n')
+    prev_articles = dict.fromkeys(prev_articles_r)
+    for a in prev_articles_r:
+        prev_articles[a] = 1
+
+    r_articles = pickle.load(open('pickle_jar/depth1_links.pkl', 'r'))
+    print('removing duplicates...')
+    for a in r_articles:
+        if (not prev_articles.get(a) and not d_articles.get(a)
+                and not poll_tally.get(a) and a[:4] =='http'):
+            d_articles[a] = 1
+    articles = d_articles.keys()
+    print('shuffling articles...')
+    np.random.shuffle(articles)
+
+    
+    print("all_articles has {} articles to crawl".format(len(articles)))
+
+
+    sublinks = []
+
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
-
     process.crawl(ElectionSpider)
     process.start() # the script will block here until the crawling is finished
+    '''
+
+    configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+    runner = CrawlerRunner()
+    d = runner.crawl(ElectionSpider)
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
+    '''
+    print("{} hits / {} crawled".format(poll_link_count, total_crawl_count))
+    pickle.dump(sublinks, open('depth2_links.pkl', 'w'))
+    pickle.dump(link_dict, open('depth1_link_dict.pkl', 'w'))
+    pickle.dump(poll_tally, open('poll_tally_depth1.pkl', 'w'))
 
 
 
 class ElectionSpider(scrapy.Spider):
     name = 'election'
+    DOWNLOAD_DELAY = 0.5
+    DOWNLOAD_HANDLERS = {
+        'file': None,
+        'http': 'scrapy.core.downloader.handlers.http.HTTPDownloadHandler',
+        'https': 'scrapy.core.downloader.handlers.http.HTTPDownloadHandler',
+        's3': None,
+        'ftp': None
+    }
+    REDIRECT_MAX_TIMES = 4
+    DOWNLOAD_TIMEOUT = 16
+    CONCURRENT_ITEMS = 4000
+    ROBOTSTXT_OBEY = True
+    DOWNLOAD_MAXSIZE = 16777216 # no more than 16MB
+    CONCURRENT_REQUESTS_PER_IP = 4
+    CONCURRENT_REQUESTS = 32
+    USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    REACTOR_THREADPOOL_MAXSIZE = 32
+
     def start_requests(self):
-        urls = [
-            'http://www.marketwatch.com/story/polls-show-clinton-widening-lead-over-trump-and-no-its-not-a-conspiracy-2016-10-19' 
-                ]
-        for url in urls:
+        global articles
+        toreturn = []
+        for start_url in articles:
+            try:
+                toreturn.append(Request(url=start_url))
+            except Exception:
+                pass
+
+        return toreturn
+        '''
+        for url in articles:
             yield scrapy.Request(url=url, callback=self.parse)
+        '''
 
     def parse(self, response):
+        global articles
+        global raw_polls
+        global raw_s_polls
+        global sublinks
+        global link_dict
+        global poll_link_count
+        global total_crawl_count
+        global poll_tally
+
         bod = response.body
-        soup = BeautifulSoup(bod, 'lxml')
-        links = []
-        for link in soup.find_all('a'):
-            print('LINK: {}'.format(link.get('href')))
+        url = response.url
+        if url in polls:
+            # a redirect eventually led to a poll
+            red_poll_link_count += 1
+            poll_tally[url] += 1
+        try:
+            poll_links = []
+            soup = BeautifulSoup(bod, 'lxml')
+            for link in soup.find_all('a'):
+                href = link.get('href')
+                if href is not None and len(href) > 0:
+                    href = href.strip()
+                    if href in polls: # link to poll!
+                        poll_link_count += 1
+                        poll_links.append(href)
+                        poll_tally[href] += 1
+                    elif 'http' in href:
+                        sublinks.append(href)
+
+            if len(poll_links) > 0 and len(poll_links) < 10:
+                link_dict[url] = poll_links
+
+            total_crawl_count += 1
+            if (total_crawl_count % 100 == 0):
+                print("\nELECTSPIDER crawled {} urls\n".format(total_crawl_count))
+
+        except Exception, e:
+            print("error with {}".format(url))
+            print(e)
 
 
 
